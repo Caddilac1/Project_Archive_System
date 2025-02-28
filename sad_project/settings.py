e387c92c8198fa12c112e38dceeb403d563be689
Caddilac1 <155891049+Caddilac1@users.noreply.github.com>
2025-02-27 22:45:25 -0800

    Fix STATIC_ROOT for Heroku

--- sad_project/settings.py
+++ sad_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,9 +130,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# This is used in development (where your static files are stored)
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR, 'static'),
 ]
+
+# This is required for deployment (where collectstatic will store all static files)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Ensure Whitenoise is configured (if using it)
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
